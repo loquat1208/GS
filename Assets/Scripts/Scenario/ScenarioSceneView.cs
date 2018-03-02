0ec60c7dc82dd0864ab9ab5abe05c9ba37298a39
LEE DONGEUN <tetares4@gmail.com>
2018-03-03 00:54:09 +0900

    [CLIENT] 시나리오 씬 대사 기능 추가

--- Assets/Scripts/Scenario/ScenarioSceneView.cs
+++ Assets/Scripts/Scenario/ScenarioSceneView.cs
@@ -7,12 +7,14 @@ namespace GS.Scenario
     public class ScenarioSceneView : MonoBehaviour
     {
         [SerializeField] private Text Name;
+        [SerializeField] private Text Line;
 
         private ScenarioSceneModel[ ] Scenes { get { return Service.Instance.ScenarioService.Scenes; } }
 
         private void Start()
         {
             Name.text = Scenes[0].Name;
+            Line.text = Scenes[0].Line;
         }
     }
 }
